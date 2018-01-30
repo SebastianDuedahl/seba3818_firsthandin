fd70bda911daa1636fceff68500ce1a4cdb1c02e
Sebastian Duedahl <seba3818@stud.kea.dk>
2018-01-30 13:59:57 +0100

    FirstHandIn

---
+++ HandIn1.java
@@ -0,0 +1,15 @@
+public class HandIn1
+{
+    public static void main(String[] args){
+
+        for (int i = 1; i < 11 ; i++)
+        {
+
+            System.out.println(i);
+
+        }
+
+    }
+
+
+}
